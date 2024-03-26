3641239defd2871bc3f4623a556e899180cf2134
kifah saloum <kifah.saloum.96@hotmail.com>
2024-03-26 18:13:02 +0300

    Responding to events

--- Week_8/Codezilla/react-basics/src/App.jsx
+++ Week_8/Codezilla/react-basics/src/App.jsx
@@ -234,3 +234,23 @@ export default function ShoppingList() {
 //* ---------------------------------------------------------------------
 
 //* >>>>> Responding to events  <<<<<
+
+/*
+function MyButton() {
+  function handleClick() {
+    alert('You clicked me!');
+  }
+  
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return <MyButton/>
+}
+*/
+
+//* ---------------------------------------------------------------------
