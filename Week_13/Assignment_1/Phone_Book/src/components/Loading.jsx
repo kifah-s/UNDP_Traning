3d602e7481003352cdfa069d09e2c7e671612d11
kifah saloum <kifah.saloum.96@hotmail.com>
2024-04-26 18:07:52 +0300

    Rename

--- Week_13/Assignment_1/Phone_Book/src/components/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import {Spinner} from "@nextui-org/react";
-
-const Loading = () => {
-    return (
-        <div className="h-full w-full flex justify-center items-center"><Spinner /></div>
-    );
-};
-
-export default Loading;
