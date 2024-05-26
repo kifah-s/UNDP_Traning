00d3106615df30e923a3270f544f12db128b234a
kifah saloum <kifah.saloum.96@hotmail.com>
2024-05-26 13:21:36 +0300

    Update my-app

--- Week_21/my-app/src/app/articles/[articleTitle]/page.jsx
+++ Week_21/my-app/src/app/articles/[articleTitle]/page.jsx
@@ -1,11 +1,10 @@
 function ShowArticlePage(props) {
   console.log(props);
-  console.log(props.params.title);
 
   return (
     <>
       <h1 className="centerElement">Show Article Page</h1>
-      <h3 >Article Id: {props.params.articleId}</h3>
+      <h3 >Article Title: {props.params.articleTitle}</h3>
     </>
   );
 }
--- Week_21/my-app/src/app/posts/featuredPosts/page.jsx
+++ Week_21/my-app/src/app/posts/featuredPosts/page.jsx
@@ -1,3 +1,8 @@
+
+export const metadata = {
+  title: "Featured Posts",
+};
+
 function FeaturedPostsPage() {
   return (
     <>
