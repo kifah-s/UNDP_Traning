2ee24d0c71569e81d4bb30630584866ac3b996dc
kifah saloum <kifah.saloum.96@hotmail.com>
2024-06-06 20:56:47 +0300

    Update

--- Week_14/todo-list/src/App.jsx
+++ Week_14/todo-list/src/App.jsx
@@ -3,21 +3,37 @@ import "./App.css";
 
 function App() {
   // * States.
-  const [todos, setTodos] = useState(["sasasa"]);
+  const [todos, setTodos] = useState([]);
 
   const inputRef = useRef();
 
   // * Functions.
-  const handleAddTodo = () => {};  
+  const handleAddTodo = () => {
+    const text = inputRef.current.value;
+    // console.log(text);
 
+    const newItem = { completed: false, text };
+
+    setTodos([...todos, newItem]);
+
+    inputRef.current.value = [""];
+  };
+
+  const handleItemDone = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  console.log(todos);
 
   return (
     <div className="App">
       <div className="container">
         <h1>Todo List</h1>
         <ol>
-          {todos.map((item) => {
-            return <li>{item}</li>;
+          {todos.map(({ text }, index) => {
+            return <li key={index} onClick={() => handleItemDone(index)}>{text}</li>; //* min: 27
           })}
         </ol>
         <input type="text" placeholder="Enter item .." ref={inputRef} />
