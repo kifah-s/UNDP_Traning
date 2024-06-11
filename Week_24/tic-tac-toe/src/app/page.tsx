5f7ffbf3de9e1dc1085b2fcd2ab93fc7b52779c3
kifah saloum <kifah.saloum.96@hotmail.com>
2024-06-11 14:59:21 +0300

    Add logic

--- Week_24/tic-tac-toe/src/app/page.tsx
+++ Week_24/tic-tac-toe/src/app/page.tsx
@@ -6,14 +6,27 @@ import { useState } from "react";
 export default function Home() {
   //* States.
   const [cells, setCells] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [go, setGo] = useState("circle");
+
+  console.log(cells);
+  
 
   return (
     <main className="container">
       <div className="gameBoard">
         {cells.map((cell, index) => (
-          <Cell key={index} />
+          <Cell
+            id={index}
+            go={go}
+            setGo={setGo}
+            key={index}
+            cells={cells}
+            setCells={setCells}
+          />
         ))}
       </div>
     </main>
   );
 }
+
+//* Min: .
--- Week_24/tic-tac-toe/src/components/cell.tsx
+++ Week_24/tic-tac-toe/src/components/cell.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import { log } from "console";
+import React, { Dispatch, SetStateAction } from "react";
 
-const Cell = () => {
+type CellProps = {
+  id: number;
+  go: string;
+  setGo: Dispatch<SetStateAction<string>>;
+  cells: string[];
+  setCells: Dispatch<SetStateAction<string>>;
+};
+
+const Cell = ({ id, go, setGo, cells, setCells }: CellProps) => {
+  //* Function.
+  const handleClick = (e) => {
+    // console.log(id);
+
+    const notTaken = !cells[id];
+    if (notTaken) {
+      if (go === "circle") {
+        handleCellChange("circle");
+        setGo("cross");
+      } else if (go === "cross") {
+        handleCellChange("cross");
+        setGo("circle");
+      }
+    }
+  };
+
+  const handleCellChange = (cellToChange: string) => {
+    let copyCells = [...cells];
+    copyCells[id] = cellToChange;
+    setCells(copyCells);
+  };
+
   return (
     <>
-      <div className="square">X</div>
+      <div className="square" onClick={handleClick}>
+        X
+      </div>
     </>
   );
 };
